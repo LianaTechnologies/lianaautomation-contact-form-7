08060580c9393888d0d3a52fc67b9fc70f5e9e6c
Jaakko Pero <jaakko.pero@lianatech.com>
2022-10-13 15:45:41 +0300

    added mandatory files for plugin directory presence

--- lianaautomation-contact-form-7.php
+++ lianaautomation-contact-form-7.php
@@ -1,24 +1,20 @@
 <?php
 /**
  * Plugin Name:       LianaAutomation - Contact Form 7
- * Plugin URI:        https://www.lianatech.com/solutions/websites
- * Description:       LianaAutomation for Contact Form 7.
- * Version:           1.0.30
+ * Description:       LianaAutomation for Contact Form 7
+ * Version:           1.0.31
  * Requires at least: 5.2
  * Requires PHP:      7.4
- * Author:            Liana Technologies
+ * Author:            Liana Technologies Oy
  * Author URI:        https://www.lianatech.com
  * License:           GPL-3.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-3.0-standalone.html
  * Text Domain:       lianaautomation
  * Domain Path:       /languages
- * 
+ *
  * PHP Version 7.4
- * 
- * @category Components
- * @package  WordPress
- * @author   Liana Technologies <websites@lianatech.com>
- * @author   Jaakko Pero <jaakko.pero@lianatech.com>
+ *
+ * @package  LianaAutomation
  * @license  https://www.gnu.org/licenses/gpl-3.0-standalone.html GPL-3.0-or-later
  * @link     https://www.lianatech.com
  */
@@ -26,14 +22,16 @@
 /**
  * Include cookie handler code
  */
-require_once dirname(__FILE__) . '/includes/lianaautomation-cookie.php';
+require_once dirname( __FILE__ ) . '/includes/lianaautomation-cookie.php';
 
 /**
  * Include Contact Form 7 code
  */
-require_once dirname(__FILE__) . '/includes/lianaautomation-wpcf7.php';
+require_once dirname( __FILE__ ) . '/includes/lianaautomation-wpcf7.php';
 
 /**
- * Include admin panel code
+ * Conditionally include admin panel code
  */
-require_once dirname(__FILE__) . '/admin/lianaautomation-admin.php';
+if ( is_admin() ) {
+	require_once dirname( __FILE__ ) . '/admin/lianaautomation-admin.php';
+}
